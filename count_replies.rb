f330e568fb478a8b221797e83ba37c6755da668c
Kerry Buckley <kerryjbuckley@gmail.com>
2013-08-31 21:34:01 +0100

    Methods returning lambdas

--- count_replies.rb
+++ count_replies.rb
@@ -4,44 +4,32 @@ require "csv"
 
 def main file
   CSV.table(file)
-    .map {|row| Tweet.new row[:text] }
-    .select(&:reply?)
-    .group_by(&:in_reply_to)
-    .map {|user, replies| Recipient.new user, replies.size }
-    .sort
+    .select(&reply?)
+    .group_by(&in_reply_to)
+    .map(&count_users)
+    .sort_by(&sort_by_count)
     .take(10)
-    .each(&:report)
+    .each(&report)
 end
 
-class Tweet
-  def initialize text
-    @text = text
-  end
-
-  def reply?
-    @text =~ /^@\w/
-  end
-
-  def in_reply_to
-    @text[/^@\w+/].downcase
-  end
+def reply?
+  ->(tweet) { tweet[:text] =~ /^@\w/ }
 end
 
-class Recipient
-  attr_reader :number_of_replies
+def in_reply_to
+  ->(reply) { reply[:text][/^@\w+/].downcase }
+end
 
-  def initialize name, number_of_replies
-    @name = name
-    @number_of_replies = number_of_replies
-  end
+def count_users
+  ->((user, replies)) { [user, replies.size] }
+end
 
-  def <=> other
-    other.number_of_replies <=> number_of_replies
-  end
+def sort_by_count
+  ->((_user, count)) { -count }
+end
 
-  def report
-    printf "%15s: %3i\n", @name, @number_of_replies
-  end
+def report
+  ->((user, count)) { printf "%15s: %3i\n", user, count }
 end
 
 main "data/tweets.csv"
